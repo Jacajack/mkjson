4f5d74441b73e641ae198dcb74ddbd3c1f77cf62
Jacek Wieczorek <mrjjot@gmail.com>
2018-02-01 12:57:08 +0100

    Update the example

--- examples/example.c
+++ examples/example.c
@@ -5,11 +5,14 @@
 
 int main( )
 {
-	char *json = mkjson( 8,
+	char *json = mkjson( 10,
 		's', "mystr", "hello world!",
 		'i', "myinteger", 42,
-		'd', "mydoublevalue", 1.4481,
+		'I', "longlong", 784ll,
+		'd', "double", 1.4481,
+		'D', "longdbl", 1.22l,
 		'e', "exponential", 3e62,
+		'E', "exponentialL", 3e104l,	
 		'b', "boolean", 1,
 		'n', "nullvalue",
 		'j', "object", mkjson( 1, 
