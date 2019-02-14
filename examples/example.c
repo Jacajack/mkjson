90a9e760b33647c463540dfcb09de60a6ae7d488
Jacek Wieczorek <mrjjot@gmail.com>
2019-02-14 12:52:48 +0100

    Remove whitespace

--- examples/example.c
+++ examples/example.c
@@ -12,20 +12,20 @@ int main( )
 		'd', "double", 1.4481,
 		'D', "longdbl", 1.22l,
 		'e', "exponential", 3e62,
-		'E', "exponentialL", 3e104l,	
+		'E', "exponentialL", 3e104l,
 		'b', "boolean", 1,
 		'n', "nullvalue",
 		'r', "alsonull", NULL,
-		'j', "object", mkjson( MKJSON_OBJ, 1, 
-			'i', "something", 45 
+		'j', "object", mkjson( MKJSON_OBJ, 1,
+			'i', "something", 45
 			),
-		'j', "array", mkjson( MKJSON_ARR, 3, 
+		'j', "array", mkjson( MKJSON_ARR, 3,
 			'i', 123,
 			's', "another string!",
 			'b', 0 ),
 		'j', "empty", mkjson( MKJSON_OBJ, 0 )
 		);
-	
+
 	//Just print it and then free
 	assert( json != NULL );
 	printf( "%s\n", json );
