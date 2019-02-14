e5fe6599439fb4f68f64e32402ffa7f78d422996
Jacek Wieczorek <mrjjot@gmail.com>
2019-02-14 13:06:07 +0100

    Update example code

--- examples/example.c
+++ examples/example.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 #include "mkjson.h"
 
 int main( )
 {
 	char *json = mkjson( MKJSON_OBJ, 13,
-		's', "mystr", "hello world!",
-		'i', "myinteger", 42,
-		'I', "longlong", 784ll,
-		'd', "double", 1.4481,
-		'D', "longdbl", 1.22l,
-		'e', "exponential", 3e62,
-		'E', "exponentialL", 3e104l,
-		'b', "boolean", 1,
-		'n', "nullvalue",
-		'r', "alsonull", NULL,
-		'j', "object", mkjson( MKJSON_OBJ, 1,
-			'i', "something", 45
+		MKJSON_STRING,      "mystr", "hello world!",
+		MKJSON_INT,         "myinteger", 42,
+		MKJSON_LLINT,       "longlong", 784ll,
+		MKJSON_DOUBLE,      "double", 1.4481,
+		MKJSON_LDOUBLE,     "longdbl", 1.22l,
+		MKJSON_SCI_DOUBLE,  "exponential", 3e62,
+		MKJSON_SCI_LDOUBLE, "exponentialL", 3e104l,
+		MKJSON_BOOL,        "boolean", 1,
+		MKJSON_NULL,        "nullvalue",
+		MKJSON_STRING,      "alsonull", NULL,
+
+		MKJSON_JSON_FREE,   "object", mkjson( MKJSON_OBJ, 1,
+			MKJSON_INT,     "something", 45
 			),
-		'j', "array", mkjson( MKJSON_ARR, 3,
-			'i', 123,
-			's', "another string!",
-			'b', 0 ),
-		'j', "empty", mkjson( MKJSON_OBJ, 0 )
-		);
+
+		MKJSON_JSON_FREE,   "array", mkjson( MKJSON_ARR, 3,
+			MKJSON_INT,         123,
+			MKJSON_STRING_FREE, strdup( "another string!" ),
+			MKJSON_BOOL,        0
+			),
+
+		MKJSON_JSON_FREE,   "empty", mkjson( MKJSON_OBJ, 0 )
+			);
 
 	//Just print it and then free
 	assert( json != NULL );
